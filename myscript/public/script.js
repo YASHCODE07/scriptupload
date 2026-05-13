ddd5a3feef06c7bd6e39fea62fd2b493f93f1eb2
YASH RAJ <yrajgecarwl@gmail.com>
2026-05-13 10:42:46 +0530

    Add files via upload

---
+++ myscript/public/script.js
@@ -0,0 +1,8 @@
+console.log("External script loaded");
+
+const p = document.createElement("p");
+
+p.innerText =
+  "This paragraph is added by external script.js";
+
+document.body.appendChild(p);
